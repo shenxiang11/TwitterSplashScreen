f8767848c91a523e96f7fd43c63f57d28bb7d247
Shen <863461783@qq.com>
2024-02-08 10:25:25 +0800

    Initial Commit

---
+++ TwitterSplashScreen/TwitterSplashScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  TwitterSplashScreenApp.swift
+//  TwitterSplashScreen
+//
+//  Created by 香饽饽zizizi on 2024/2/8.
+//
+
+import SwiftUI
+
+@main
+struct TwitterSplashScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
