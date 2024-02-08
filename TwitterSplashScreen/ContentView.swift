5e39aa91b808a6ad3e3ed6d7b5d68150e706ff7a
Shen <863461783@qq.com>
2024-02-08 10:59:41 +0800

    first commit

--- TwitterSplashScreen/ContentView.swift
+++ TwitterSplashScreen/ContentView.swift
@@ -8,14 +8,39 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShown = false
+
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ScrollView {
+            VStack(spacing: 20) {
+                ForEach(1...100, id: \.self) { _ in
+                    RoundedRectangle(cornerRadius: 25, style: .continuous)
+                        .fill(.gray.opacity(0.3))
+                        .frame(height: 100)
+                }
+            }
+            .padding()
         }
-        .padding()
+        .frame(maxWidth: .infinity, maxHeight: .infinity)
+        .overlay {
+            if !isShown {
+                Color.bg
+                    .mask {
+                        Color.bg
+                            .overlay {
+                                Image(.logo)
+                                    .resizable()
+                                    .frame(width: 80, height: 80)
+                                    .blendMode(.destinationOut)
+                            }
+                    }
+                    .ignoresSafeArea()
+                    .transition(.scale(scale: 40).animation(.easeInOut(duration: 1)))
+            }
+        }
+        .onAppear {
+            isShown = true
+        }
     }
 }
 
